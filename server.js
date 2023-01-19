ec7d06582ec65825ab0cc1159a2b86ec4e818c15
Seezee23 <seezee146@yahoo.com>
2023-01-18 20:07:51 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+const mockBlog = require("./data")
+app.use(express.json());
+
+app.get('/',(req, res) => {
+    res.json(mockBlog)
+})
+
+app.get("/:id",(req, res) => {
+    res.json(mockBlog.find(post => post.id == req.params.id))
+})
+
+app.post("/", (req, res) => {
+    res.json(req.body)
+})
+
+app.listen(3008,() =>{
+    console.log("listening on port 3008")
+})
